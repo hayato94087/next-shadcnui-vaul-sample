839ca88dc2a25b0a0fb390c9918a55b417327bf7
hayato94087 <hayato94087@gmail.com>
2024-01-01 05:00:10 +0900

    feat:Drawerコンポーネントを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { DrawerDialogDemo } from "@/components/drawer-dialog-demo";
 import { type FC } from "react";
 
 const Home: FC = () => {
@@ -8,6 +9,7 @@ const Home: FC = () => {
         <span className="text-blue-500">Hello</span>
         <span className="text-red-500">World</span>
       </div>
+      <DrawerDialogDemo />
     </div>
   );
 };
